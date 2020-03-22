9873a688a6b94240421f2b4b918c5c5728488f47
Evgeni Koliarou <ml_rey@mail.ru>
2020-03-22 21:17:26 +0300

    feat: solve

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
+  
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  let arrayJoin = bracketsConfig.map((e)=> e.join(''));
+  
+  for (let i = 0; i < arrayJoin.length; i++) {
+    if (str.includes(arrayJoin[i]) == true) {
+      str = str.replace(arrayJoin[i], '');
+      i = -1;
+    }
+  }
+  return Object.is(str, '');
 }
